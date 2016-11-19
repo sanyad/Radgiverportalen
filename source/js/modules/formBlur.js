3e1c44d9cc313679537dd1f74ef0da92d22c21f8
Alex_d <alexdwork9@gmail.com>
2016-11-19 09:17:23 +0200

    fix main page

--- source/js/modules/formBlur.js
+++ source/js/modules/formBlur.js
@@ -4,24 +4,33 @@ var page = $(window).height();
 
 
 function resizedw290() {
-    $('.fb-container').html('<div class="fb-page" data-href="https://www.facebook.com/www.spreknorge.no" data-tabs="timeline" data-width="290" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/www.spreknorge.no" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/www.spreknorge.no">Sprek Norge</a></blockquote></div>');
+    $('.fb-container').html('<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="290" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>');
     FB.XFBML.parse();
 }
-function resizedw370() {
-    $('.fb-container').html('<div class="fb-page" data-href="https://www.facebook.com/www.spreknorge.no" data-tabs="timeline" data-width="370" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/www.spreknorge.no" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/www.spreknorge.no">Sprek Norge</a></blockquote></div>');
+function resizedw375() {
+    $('.fb-container').html('<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="375" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>');
+    FB.XFBML.parse();
+}
+function resizedw410() {
+    $('.fb-container').html('<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="410" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>');
     FB.XFBML.parse();
 }
 
 $(window).resize (function () {
-    if ($(window).width() <= 991) {
+    if ($(window).width() <= 767) {
         clearTimeout(doit);
         doit = setTimeout(function () {
             resizedw290();
         }, 500);
-    } else if ($(window).width() > 991) {
+    } else if ($(window).width() > 991 && $(window).width() < 1200) {
         clearTimeout(doit);
         doit = setTimeout(function () {
-            resizedw370();
+            resizedw375();
+        }, 500);
+    } else if ($(window).width() > 1199) {
+        clearTimeout(doit);
+        doit = setTimeout(function () {
+            resizedw410();
         }, 500);
     }
 
